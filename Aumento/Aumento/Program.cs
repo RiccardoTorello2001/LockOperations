ebe0f2ee47ba27d4dbdbb6632b26cbbfcbd06410
Riccardo Torello <rikitorello2001@gmail.com>
2020-03-11 11:28:31 +0100

    Creazione di un programma tramite la parola chiave lock

---
+++ Aumento/Aumento/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aumento
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = 0;
+			object padlock = new object();
+
+			for (int i = 0; i < 5; i++)
+			{
+				Task conta = new Task(() =>
+				{
+					//Monitor
+					//Monitor.Enter(padlock);
+					//try
+					//{
+					//    n++;
+					//    Thread.Sleep(1);
+					//    Console.WriteLine($"n = {n}");
+					//}
+					//finally
+					//{
+					//    Monitor.Exit(padlock);
+					//}
+
+					//lock
+					lock (padlock)
+					{
+						n++;
+						Thread.Sleep(1);
+						Console.WriteLine($"n = {n}");
+					}
+
+				});
+				conta.Start();
+			}
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Aumento/Lockoperation/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lockoperation
+{
+	class Program
+	{
+
+		static double _val1 = 1, _val2 = 2;
+
+		//token
+		static object padlock = new object();
+		static void Go()
+		{
+			lock (padlock)
+			{
+				//sezione critica (istruzione lavora su una variabile condivisa che deve lavorare in mutua esclusione)
+				if (_val2 != 0)
+				{
+					Console.WriteLine(_val1 / _val2);
+					_val2 = 0;
+				}
+				else
+					Console.WriteLine("No");
+			}
+		}
+
+		static void Go2()
+		{
+			Monitor.Enter(padlock);
+			try
+			{
+				//sezione critica (istruzione lavora su una variabile condivisa che deve lavorare in mutua esclusione)
+
+				Console.WriteLine(_val1 / _val2);
+				_val2 = 0;
+			}
+			catch
+			{
+				Console.WriteLine("operazione non eseguibile");
+			}
+			finally
+			{
+				Monitor.Exit(padlock);
+			}
+
+		}
+		static void Main(string[] args)
+		{
+			//Task.Factory.StartNew(Go);
+			//Task.Factory.StartNew(Go);
+			//Task.Factory.StartNew(Go);
+
+			Task.Factory.StartNew(Go2);
+			Task.Factory.StartNew(Go2);
+			Task.Factory.StartNew(Go2);
+
+			Console.ReadLine();
+		}
+	}
+}
